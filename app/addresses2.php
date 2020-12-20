18855569ef26899c207326717f30a22b3bc8799b
Saif Hatem <saifxhatem@gmail.com>
2020-12-20 11:42:27 +0200

    added addresses migration

---
+++ app/addresses2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class addresses2 extends Model
+{
+    protected $primaryKey = 'id'; //unnecessary
+    protected $connection = 'mysql'; //unnecessary as well
+}
